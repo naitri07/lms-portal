a2c890e83dd0d0ee2740f74559ab2cbe2a37195e
naitri07 <chavannaitri@gmail.com>
2026-07-22 11:41:19 +0530

    Complete Frontend

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg'
-import logo_dark from './logo_dark.svg'
+import logo from './CogniShift-logo.png'
+import logo_dark from './CogniShift-logo.png'
 import search_icon from './search_icon.svg'
 import cross_icon from './cross_icon.svg'
 import upload_area from './upload_area.svg'
